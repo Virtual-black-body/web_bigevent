604901bef99dfb612eda1cd60419aee3dfab6d15
Virtual-black-body <1623429577@qq.com>
2021-09-26 00:52:19 +0800

    完成了文章部分的功能

--- assets/js/article/art_list.js
+++ assets/js/article/art_list.js
@@ -2,7 +2,6 @@ $(function() {
 
     var layer = layui.layer
     var form = layui.form
-
     var q = {
         pagenum: 1,
         pagesize: 2,
@@ -43,6 +42,8 @@ $(function() {
                 }
                 var htmlstr = template('tpl-table', res)
                 $('tbody').html(htmlstr)
+
+                renderPage(res.total)
             }
         })
     }
@@ -63,4 +64,88 @@ $(function() {
             }
         })
     }
+
+    $('#form-search').on('submit', function(e) {
+        e.preventDefault()
+
+        var cate_id = $('[name=cate_id]').val()
+        var state = $('[name=state]').val()
+        q.state = state
+        q.cate_id = cate_id
+        initTable()
+    })
+    // 渲染分页控件
+    function renderPage(total) {
+        layui.laypage.render(
+            {
+                elem: 'pageBox', //注意，这里的 test1 是 ID，不用加 # 号
+                count: total,
+                layout: ['count','limit', 'prev', 'page', 'next','skip'],
+                limits: [2, 3, 5, 10, 20],
+                limit: q.pagesize,
+                curr: q.pagenum,
+                jump: function(obj, first){
+                    q.pagenum = obj.curr
+                    if(!first){
+                        initTable()
+                     }
+                }
+            }
+        )
+    }
+
+    $('tbody').on('click', '#btn-delete', function (res){
+        var id = $(this).attr('data-id')
+        var len = $('.btn-delete').length
+        layer.confirm('确认删除？', {icon: 3, title:'提示'}, function(index){
+            //do something
+            
+            $.ajax({
+                method: 'GET',
+                url: '/my/article/delete/' + id,
+                success: function(res) {
+                    if(res.status !== 0){
+                        return layer.msg('删除文章失败！')
+                    }
+                    layer.msg('删除文章成功！')
+
+                    if( len === 1 ) {
+                        q.pagenum = q.pagenum === 1 ? 1 : q.pagenum - 1
+                    }
+                    
+                    initTable()
+                }
+            })
+            layer.close(index);
+          });
+    })
+
+    $('tbody').on('click', '#btn-edit', function() {
+        var id = $(this).attr('data-id')
+        var data = null
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/'+ id ,
+            success: function(res) {
+                if(res.status !== 0) {
+                    return layer.msg('获取文章详情失败！')
+                }
+                data = res.data
+                
+            }
+        })
+        indexAdd = layer.open({
+            type: 2,
+            area: ['840px', '600px'],
+            title: '修改文章类别'
+            ,content: '/article/art_edit.html',
+            success: function(layero,index){
+                var sbody = $($(layero).find('iframe')[0].contentDocument.body)
+                sbody.find('[name = Id]').val(data.Id)
+                sbody.find('[name = title]').val(data.title)
+                sbody.find('[name = index]').val(index)
+              }
+          })
+        
+    })
 })
